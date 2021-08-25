211b5d1cace89c677416291feb21ff31140e7b06
Zaxser <nerlenemaedavis@gmail.com>
2021-08-25 14:55:44 +0800

    Messing with the batch file

--- misc/build.bat
+++ misc/build.bat
@@ -1,12 +1,12 @@
-REM This is just the path of vcvarsall on my machine. Your mileage may vary.
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall" x64
+rem This is just the path of vcvarsall on my machine. Your mileage may vary.
 
-REM %~dp0 resolves to the path of build.bat; this makes it so that build.bat can
-REM can be called from anywhere and still work.
-rm -r "%~dp0/../build"
-mkdir "%~dp0/../build"
-pushd "%~dp0/../build"
-cl -FC -Zi ../code/win32_handmade.cpp user32.lib gdi32.lib
+rem %~dp0 resolves to the path of build.bat; this makes it so that build.bat can
+rem can be called from anywhere and still work.
+rmdir /Q /S "%~dp0..\build\"
+mkdir "%~dp0..\build"
+pushd "%~dp0..\build"
+cl -FC -Zi ..\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
 
-call "%~dp0/../build/win32_handmade.exe"
+call "%~dp0..\build\win32_handmade.exe"
